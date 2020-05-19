967af7f1aa1946bdc7c4cba0604ce6cda3ec7c5d
qinzhenpeng <839858321@qq.com>
2020-05-19 14:07:20 +0800

    新增springboot

---
+++ demo/src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import java.util.stream.Stream;
+
+public class Test {
+    public static void main(String[] args) {
+        Stream<String> s = Stream.of("test", "t1", "t2", "teeeee", "aaaa");
+//查找所有包含t的元素并进行打印
+        s.filter(n -> n.contains("t")).forEach(System.out::println);
+    }
+}
